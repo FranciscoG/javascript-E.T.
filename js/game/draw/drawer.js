25edbfbfade12dd73fab905c40d61f9212f467ff
FranciscoG <frankjg@gmail.com>
2014-08-04 17:48:16 -0400

    updating comments with better link

--- js/game/draw/drawer.js
+++ js/game/draw/drawer.js
@@ -1,15 +1,11 @@
 /*
-	some helpful explanations of the 2600 graphics 
-
-	http://atariage.com/forums/topic/169128-what-is-the-atari-2600-screen-resolution/
-	http://wayofthepixel.net/index.php?topic=13697.0
-	
+	A very helpful tutorial on how the 2600 works
+	http://atariage.com/forums/topic/33233-sorted-table-of-contents/
  */
 var spriteDrawer = function(options, sprite) {
 
   var _opts = options || {};
-  _opts.ratio = _opts.ratio || "4:3";
-  _opts.canvasWidth = _opts.canvasWidth || "320";
+  _opts.canvasWidth = _opts.canvasWidth || "160";
   _opts.canvasHeight = _opts.canvasHeight || "192";
   _opts.canvasId = _opts.canvasId || null;
 
